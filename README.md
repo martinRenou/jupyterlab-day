cec3ce37508d7eba3f8b1a6682c7759a36584e48
martinRenou <martin.renou@gmail.com>
2022-10-24 11:36:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# jupyterlab-day
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/martinRenou/jupyterlab-day/main?urlpath=lab)
+
+JupyterLab dark theme inspired by the Github dark theme
+
+## Requirements
+
+* JupyterLab >= 3.0
+
+## Install
+
+To install the extension, execute:
+
+```bash
+conda install jupyterlab-day -c conda-forge
+```
+
+or
+
+```bash
+pip install jupyterlab-day
+```
+
+![jupyterlab-day](jupyterlab-day.png)
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab-day extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-day:plugin',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    const style = 'jupyterlab-day/index.css';
+
+    manager.register({
+      name: 'JupyterLab Day',
+      isLight: false,
+      themeScrollbars: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
